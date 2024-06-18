4faf9c9f5877e7938889ac4a36cc997ef91ebf97
lauvillacis <lauvillacisd@gmail.com>
2024-06-17 18:38:27 -0600

    feat - tabla con nulos

--- cod/SQL/Archivosql.sql
+++ cod/SQL/Archivosql.sql
@@ -1,2 +1,15 @@
-SELECT *
-FROM Iris
+
+SELECT
+  t.Id,
+  t.Species,
+  [Petal.Width] AS petal_w,
+  CASE 
+    WHEN t.Id LIKE '%9%' THEN NULL
+    ELSE [Petal.Width]
+  END AS Petal_width
+FROM (
+  SELECT 
+    ROWNUMBER() OVER(ORDER BY(SELECT NULL)) AS Id,
+    *
+  FROM Iris) as t
+
